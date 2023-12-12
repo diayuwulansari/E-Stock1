27c4785ac87d8741b9c82f536538cf11c6eba1b2
HanunAzizah <azahraarreta@gmail.com>
2023-12-12 15:50:28 +0700

    category, product cushion, product blush on

---
+++ datass.js
@@ -0,0 +1,71 @@
+const datas = [
+  {
+    id: 1,
+    
+    title:
+      "Madame Gie Blushed - Makeup Blush On | 01 Funfetti",
+    image:
+      "https://down-id.img.susercontent.com/file/id-11134207-7r98x-lnb3o3ty2lmpf5",
+    recentText:
+      "jumlah product : 10", 
+   
+  },
+  {
+    id: 2,
+    
+    title: "Madame Gie Blushed - Makeup Blush On | 02 Stardust",
+    image:
+      "https://down-id.img.susercontent.com/file/id-11134207-7r98x-lnb3o5fxprba57",
+    recentText:
+      "jumlah product : 10",
+    
+  },
+  {
+    id: 3,
+    
+    title:
+      "Madame Gie Blushed - Makeup Blush On | 03 Euphoria",
+    image:
+      "https://down-id.img.susercontent.com/file/id-11134207-7r991-lnb3o84rs1gb26",
+    recentText:
+      "jumlah product : 10",
+    
+  },
+  {
+    id: 4,
+    
+    title: "Madame Gie Blushed - Makeup Blush On | 04 Dance Floor",
+    image:
+      "https://down-id.img.susercontent.com/file/id-11134207-7r98x-lnb3o9e9xg210d",
+      recentText:
+      "jumlah product : 10",
+    
+  },
+  {
+    id: 5,
+    
+    title:
+      "Madame Gie Madame Juicy Cheek - Make Up Blush On | Peachy Peach",
+    image:
+      "https://down-id.img.susercontent.com/file/f81887366ce62e177be689752f5bf44a",
+      recentText:
+      "jumlah product : 10",
+    
+  },
+  {
+    id: 6,
+    
+    title:
+      "Madame Gie Madame Juicy Cheek - Make Up Blush On | Venetian Rose",
+    image:
+      "https://down-id.img.susercontent.com/file/8436b547356855c93f27598a0462afa4",
+      recentText:
+      "jumlah product : 10",
+    
+  },
+  
+    
+  ];
+
+
+export default datas;
