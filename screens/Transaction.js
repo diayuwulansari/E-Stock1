d2c16270beb4d81c655397ae033a90fbca213875
glori76 <glori76@gmail.com>
2023-12-28 13:36:02 +0700

    perubahan halaman transaksi

--- screens/Transaction.js
+++ screens/Transaction.js
@@ -1,77 +1,110 @@
-import React, { useState } from 'react';
-import {
-  View,
-  Text,
-  Button,
-} from 'react-native';
+import { VStack, Card, Image, FormControl, Text, Input, Button, ScrollView, Select, CheckIcon  } from "native-base";
+import { Header } from "../components";
+import { useNavigation } from "@react-navigation/native";
+import { getFirestore, collection, addDoc, updateDoc, doc } from "firebase/firestore";
+import { useState, useEffect } from "react";
+import app from "../src/utils/firebase";
 
-const InputData = () => {
-  const [transaksi, setTransaksi] = useState([
-    {
-      nama: 'Lips',
-      jumlah: 0,
-    },
-    {
-      nama: 'Cushion',
-      jumlah: 0,
-    },
-    {
-      nama: 'Blush On',
-      jumlah: 0,
-    },
-    {
-      nama: 'Eyebrow',
-      jumlah: 0,
-    },
-    {
-      nama: 'Foundation',
-      jumlah: 0,
-     },
-    {
-      nama: 'Sunscreen',
-      jumlah: 0,
-    },
-    {
-      nama: 'Mascara',
-      jumlah: 0,
-    },
-    {
-      nama: 'Nail Polish',
-      jumlah: 0,
-    },
-    {
-      nama: 'Concealer',
-      jumlah: 0,
-    },
-  ]);
+
+const Transaction = ({route}) => {
+  const navigation = useNavigation();
+  const {name, id, jumlah} = route.params
+  const [isLoading, setIsLoading] = useState(false)
+  const [jumlahProduk, setJumlahProduk] = useState(0)
+  const [jenisTransaksi, setJenisTransaksi] = useState("")
+  useEffect(() => {
+    const db = getFirestore(app)
+    return navigation.addListener("focus", () => {
+      setIsLoading(false)
+      setJumlahProduk(0)
+      setJenisTransaksi("")
+    });
+  },[navigation])
+
+  const submitTransaction = async () => {
+    if (jenisTransaksi == "") return alert("Pilih jenis transaksi terlebih dahulu !!")
+    if (jumlah < jumlahProduk) return alert("Jumlah tidak boleh lebih besar dari stock !!")
+    setIsLoading(true)
+    const db = getFirestore(app)
+    const setChangeProduct = await updateDoc(doc(db, products/${id}), {qty: jenisTransaksi == "Masuk" ? jumlah + jumlahProduk : jumlah - jumlahProduk})
+    const addToDatabase = await addDoc(collection(db, "transactions"), {
+      productId: id,
+      qty: jumlahProduk,
+      type: jenisTransaksi
+    });
+    if (addToDatabase) {navigation.navigate("Home");setIsLoading(false)}
+  }
 
   return (
-    <View style={{ flex: 1 }}>
-      <View style={{ backgroundColor: 'white', padding: 10 }}>
-        <Text style={{ color: 'black', fontSize: 20 }}>Transaksi</Text>
-      </View>
-      <View style={{ width: '100%', borderTopWidth: 1, borderTopColor: '#000' }}>
-        <View style={{ flexDirection: 'row', alignItems: 'center', padding: 10 }}>
-          <Text style={{ width: '50%' }}>Nama</Text>
-          <Text style={{ width: '50%' }}>Jumlah</Text>
-        </View>
-        {transaksi.map((item) => (
-          <View key={item.nama} style={{ flexDirection: 'row', alignItems: 'center', padding: 15 }}>
-            <Text style={{ width: '50%' }}>{item.nama}</Text>
-            <Text style={{ width: '50%' }}>{item.jumlah}</Text>
-          </View>
-        ))}
-      </View>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
-      <Button title="Masuk" onPress={() => {
-  setTransaksi(transaksi.map((item) => {
-    return { ...item, jumlah: item.jumlah + 1 };
-  }));
-}} />
-        <Button title="Keluar" onPress={() => { setTransaksi([...transaksi, { nama: 'Lips', jumlah: 0 }]); }} />
-      </View>
-    </View>
+    <>
+      <Image
+      source={require('../assets/bdAdd.jpg')}
+      alt="Alternate Text"
+      size="100%"
+      resizeMode="cover"
+      style={{ position: 'absolute', zIndex: -1 }}
+    />
+
+      <Header title={"Transaction"} />
+      <VStack space={1} alignItems="center">
+        <Card w="320" h="700" bg="white" rounded="md" marginTop={10} shadow={3} p={5}>
+          <ScrollView>
+            <Text textAlign={"center"} fontWeight={"bold"} fontSize={20} marginTop={10}>Transaction</Text>
+            <FormControl style={{ width: '70%', marginLeft: 40, marginTop: 10 }}>
+              <FormControl.Label>Nama Produk</FormControl.Label>
+              <Input
+                style={{ paddingLeft: 10 }}
+                value={name}
+                isReadOnly
+              />
+            </FormControl>
+
+            <FormControl style={{ width: '70%', marginLeft: 40, marginTop: 10 }}>
+              <FormControl.Label>Jumlah Produk</FormControl.Label>
+              <Input
+                placeholder="Masukkan jumlah produk" 
+                style={{paddingLeft: 10 }}
+                keyboardType="numeric"
+                value={jumlahProduk}
+                onChangeText={(text) => setJumlahProduk(parseInt(text))}
+              />
+            </FormControl>
+
+            <FormControl style={{ width: '70%', marginLeft: 40, marginTop: 20 }}>
+              <FormControl.Label>Jenis Transaksi</FormControl.Label>
+              <Select 
+                minWidth="200" 
+                accessibilityLabel="Pilih Kategori" 
+                placeholder="Pilih Kategori" 
+                _selectedItem={{
+                  bg: "teal.600",
+                  endIcon: <CheckIcon size="5" />
+                }} 
+                mt={1} 
+                onValueChange={(text) => setJenisTransaksi(text)}
+                selectedValue={jenisTransaksi}
+              >
+                <Select.Item label="Masuk" value="Masuk" />
+                <Select.Item label="Keluar" value="Keluar" />
+              </Select>
+            </FormControl>
+
+            <Button 
+              mt={4} 
+              style={{ 
+                width: '70%', 
+                marginLeft: 40 
+              }}
+              onPress={submitTransaction}
+              isDisabled={isLoading}
+            >
+              Simpan
+            </Button>
+          </ScrollView>
+        </Card>
+      </VStack>
+    </>
   );
 };
 
-export default InputData;
+export default Transaction;
